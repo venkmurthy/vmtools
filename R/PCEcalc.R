0d530ef59f1a09ac9e0e01c1477da4f6211e402e
Venkatesh L. Murthy <venkmurthy@gmail.com>
2018-09-21 12:28:23 -0400

    Allow redefining sex and race default coding

--- R/PCEcalc.R
+++ R/PCEcalc.R
@@ -4,7 +4,7 @@
 #' Reference: Goff DC, et al. Circulation 2014;19:S49-S73
 #' DOI: 10.1161/01.cir.0000437741.48606.98
 #' @param age age in years
-#' @param race 0=white/1=black
+#' @param race white (non-black) vs. black
 #' @param sex 0=female/1=male
 #' @param tc total cholesterol in mg/dl
 #' @param hdl HDL cholesterol in mg/dl
@@ -12,9 +12,12 @@
 #' @param bptx treatment for blood pressure 0=FALSE/1=TRUE
 #' @param smoking active smoking 0=FALSE/1=TRUE
 #' @param dm diabetes mellitus 0=FALSE/1=TRUE
+#' @param white value which indicates white/non-black the parameter race (default=0)
+#' @param black value which indicates blacks in the parameter race (default=1)
+#' @param female value which indicates female in the parameter sex (default=0)
+#' @param male value which indicates male in the parameter sex (default=1)
 #' @return PCE risk as numeric from 0 to 1
 #' @keywords PCE
-#' @export
 #' @examples
 #' calcPCE(55,0,0,213,50,120,0,0,0)
 #' calcPCE(55,1,0,213,50,120,0,0,0)
@@ -29,14 +32,15 @@
 #'
 #' test.dat$PCE <- calcPCE(test.dat$age,test.dat$race,test.dat$sex,test.dat$tc,test.dat$hdl,test.dat$sbp,
 #'                         test.dat$bptx,test.dat$smoking,test.dat$dm)
+#' @export
 
-calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm) {
+calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=0, black=1, female=0, male=1) {
 
   # Initialize vector
   pce <- rep(NA,length(age))
 
   # White women
-  ww <- sex==0 & race==0
+  ww <- sex==female & race==white
   pce[ww] <- 29.18 +
     (-29.799 * log(age[ww])) + (4.884 * log(age[ww])^2) +
     (13.540 * log(tc[ww])) + (-3.114 * log(age[ww]) * log(tc[ww])) + (-13.578 * log(hdl[ww])) +
@@ -45,7 +49,7 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm) {
     (-1.665 * log(age[ww]) * smoking[ww]) + (0.661 * dm[ww])
 
   # Black women
-  bw <- sex==0 & race==1
+  bw <- sex==female & race==white
   pce[bw] <- -86.61 +
     (17.114 * log(age[bw])) + (0.940 * log(tc[bw])) + (-18.920 * log(hdl[bw])) +
     (4.475 * log(age[bw]) * log(hdl[bw])) + (29.291 * log(sbp[bw]) * bptx[bw]) +
@@ -55,7 +59,7 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm) {
     (0.691 * smoking[bw]) + (0.874 * dm[bw])
 
   # White men
-  wm <- sex==1 & race==0
+  wm <- sex==male & race==white
   pce[wm] <- -61.18 +
     (12.344 * log(age[wm])) + (11.853 * log(tc[wm])) + (-2.664 * log(age[wm]) * log(tc[wm])) +
     (-7.990 * log(hdl[wm])) + (1.769 * log(age[wm]) * log(hdl[wm])) + (1.797 * log(sbp[wm]) * bptx[wm]) +
@@ -63,7 +67,7 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm) {
     (-1.795 * log(age[wm]) * smoking[wm]) + (0.658 * dm[wm])
 
   # Black men
-  bm <- sex==1 & race==1
+  bm <- sex==white & race==black
   pce[bm] <- -19.54 +
     (2.469 * log(age[bm])) + (0.302 * log(tc[bm])) +
     (-0.307 * log(hdl[bm])) + (1.916 * log(sbp[bm]) * bptx[bm]) +
