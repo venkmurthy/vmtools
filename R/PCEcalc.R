dc64efd5e6f0f25e763a527e2e05c6e63ac9fae7
Venkatesh L. Murthy <venkmurthy@gmail.com>
2018-09-21 13:16:50 -0400

    Add multiple matches for sex, race, bptx, smoking, dm

--- R/PCEcalc.R
+++ R/PCEcalc.R
@@ -12,10 +12,16 @@
 #' @param bptx treatment for blood pressure 0=FALSE/1=TRUE
 #' @param smoking active smoking 0=FALSE/1=TRUE
 #' @param dm diabetes mellitus 0=FALSE/1=TRUE
-#' @param white value which indicates white/non-black the parameter race (default=0)
-#' @param black value which indicates blacks in the parameter race (default=1)
-#' @param female value which indicates female in the parameter sex (default=0)
-#' @param male value which indicates male in the parameter sex (default=1)
+#' @param white values which indicate white/non-black the parameter race, case-insensitive (default=0,"w","white")
+#' @param black values which indicate blacks in the parameter race, case-insensitive (default=1,"b","black")
+#' @param female values which indicate female in the parameter sex, case-insensitive (default=0,"f","female")
+#' @param male values which indicate male in the parameter sex, case-insensitive (default=1,"m","male")
+#' @param bptx.true values which indicate BP med use, case-insensitive (default=1,"t","true","y","yes")
+#' @param bptx.false values which indicate no BP med use, case-insensitive (default=0,"f","false","n","no")
+#' @param smoking.true values which indicate active smoking, case-insensitive (default=1,"t","true","y","yes","active")
+#' @param smoking.false values which indicate no active smoking, case-insensitive (default=0,"f","false","n","no","former","non-smoker","nonsmoker")
+#' @param dm.true values which indicate diabetes, case-insensitive (default=1,"t","true","y","yes")
+#' @param dm.false values which indicate nodiabetes, case-insensitive (default=0,"f","false","n","no")
 #' @return PCE risk as numeric from 0 to 1
 #' @keywords PCE
 #' @examples
@@ -27,52 +33,67 @@
 #'
 #' test.dat <- data.frame(age=rep(56:65,4),race=c(rep(0,10),rep(1,10),rep(0,10),rep(1,10)),
 #'                        sex=c(rep(0,20),rep(1,20)),tc=rep(136:145,4),hdl=rep(46:55,4),sbp=rep(126:135,4),
-#'                        bptx=rep(c(rep(0,5),rep(1,5)),4),smoking=rep(c(rep(0,5),rep(1,5)),4),
-#'                        dm=rep(c(rep(0,5),rep(1,5)),4))
+#'                        bptx=rep(c(rep(0,5),rep(1,5)),4),smoking=rep(c(rep(0,5),rep("active",5)),4),
+#'                        dm=rep(c(rep("No",5),rep(1,5)),4))
 #'
 #' test.dat$PCE <- calcPCE(test.dat$age,test.dat$race,test.dat$sex,test.dat$tc,test.dat$hdl,test.dat$sbp,
 #'                         test.dat$bptx,test.dat$smoking,test.dat$dm)
 #' @export
 
-calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=0, black=1, female=0, male=1) {
+calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=c(0,"w","white"),
+                    black=c(1,"b","black"), female=c(0,"f","female"),male=c(1,"m","male"),
+                    bptx.true=c(1,"t","true","y","yes"), bptx.false=c(0,"f","false","n","no"),
+                    smoking.true=c(1,"t","true","y","yes","active"),
+                    smoking.false=c(0,"f","false","former","non-smoker","nonsmoker"),
+                    dm.true=c(1,"t","true","y","yes"),dm.false=c(0,"f","false","n","no")) {
 
-  # Initialize vector
+
+  # Initialize vectors
   pce <- rep(NA,length(age))
+  bprx <- rep(NA,length(bptx))
+  bprx[sapply(bptx,tolower) %in% sapply(bptx.true,tolower)] <- 1
+  bprx[sapply(bptx,tolower) %in% sapply(bptx.false,tolower)] <- 0
+  smoke <- rep(NA,length(smoking))
+  smoke[sapply(smoking,tolower) %in% sapply(smoking.true,tolower)] <- 1
+  smoke[sapply(smoking,tolower) %in% sapply(smoking.false,tolower)] <- 0
+  diab <- rep(NA,length(dm))
+  diab[sapply(dm,tolower) %in% sapply(dm.true,tolower)] <- 1
+  diab[sapply(dm,tolower) %in% sapply(dm.false,tolower)] <- 0
 
   # White women
-  ww <- sex==female & race==white
+  ww <- (sapply(sex,tolower) %in% sapply(female,tolower)) & (sapply(race,tolower) %in% sapply(white,tolower))
   pce[ww] <- 29.18 +
     (-29.799 * log(age[ww])) + (4.884 * log(age[ww])^2) +
     (13.540 * log(tc[ww])) + (-3.114 * log(age[ww]) * log(tc[ww])) + (-13.578 * log(hdl[ww])) +
-    (3.149 * log(age[ww]) * log(hdl[ww])) + (2.019 * log(sbp[ww]) * bptx[ww]) +
-    (1.957 * log(sbp[ww]) * as.numeric(bptx[ww]==0)) + (7.574 * smoking[ww]) +
-    (-1.665 * log(age[ww]) * smoking[ww]) + (0.661 * dm[ww])
+    (3.149 * log(age[ww]) * log(hdl[ww])) + (2.019 * log(sbp[ww]) * bprx[ww]) +
+    (1.957 * log(sbp[ww]) * as.numeric(bprx[ww]==0)) + (7.574 * smoke[ww]) +
+    (-1.665 * log(age[ww]) * smoke[ww]) + (0.661 * diab[ww])
 
   # Black women
-  bw <- sex==female & race==white
+  bw <- (sapply(sex,tolower) %in% sapply(female,tolower)) & (sapply(race,tolower) %in% sapply(black,tolower))
   pce[bw] <- -86.61 +
     (17.114 * log(age[bw])) + (0.940 * log(tc[bw])) + (-18.920 * log(hdl[bw])) +
-    (4.475 * log(age[bw]) * log(hdl[bw])) + (29.291 * log(sbp[bw]) * bptx[bw]) +
-    (-6.432 * log(age[bw]) * log(sbp[bw]) * bptx[bw]) +
-    (27.820 * log(sbp[bw]) * as.numeric(bptx[bw]==0)) +
-    (-6.087 * log(age[bw]) * log(sbp[bw]) * as.numeric(bptx[bw]==0)) +
-    (0.691 * smoking[bw]) + (0.874 * dm[bw])
+    (4.475 * log(age[bw]) * log(hdl[bw])) + (29.291 * log(sbp[bw]) * bprx[bw]) +
+    (-6.432 * log(age[bw]) * log(sbp[bw]) * bprx[bw]) +
+    (27.820 * log(sbp[bw]) * as.numeric(bprx[bw]==0)) +
+    (-6.087 * log(age[bw]) * log(sbp[bw]) * as.numeric(bprx[bw]==0)) +
+    (0.691 * smoke[bw]) + (0.874 * diab[bw])
 
   # White men
-  wm <- sex==male & race==white
+  wm <- (sapply(sex,tolower) %in% sapply(male,tolower)) & (sapply(race,tolower) %in% sapply(white,tolower))
   pce[wm] <- -61.18 +
     (12.344 * log(age[wm])) + (11.853 * log(tc[wm])) + (-2.664 * log(age[wm]) * log(tc[wm])) +
-    (-7.990 * log(hdl[wm])) + (1.769 * log(age[wm]) * log(hdl[wm])) + (1.797 * log(sbp[wm]) * bptx[wm]) +
-    (1.764 * log(sbp[wm]) * as.numeric(bptx[wm]==0)) + (7.837 * smoking[wm]) +
-    (-1.795 * log(age[wm]) * smoking[wm]) + (0.658 * dm[wm])
+    (-7.990 * log(hdl[wm])) + (1.769 * log(age[wm]) * log(hdl[wm])) + (1.797 * log(sbp[wm]) * bprx[wm]) +
+    (1.764 * log(sbp[wm]) * as.numeric(bprx[wm]==0)) + (7.837 * smoke[wm]) +
+    (-1.795 * log(age[wm]) * smoke[wm]) + (0.658 * diab[wm])
 
   # Black men
-  bm <- sex==white & race==black
+  bm <- (sapply(sex,tolower) %in% sapply(male,tolower)) & (sapply(race,tolower) %in% sapply(black,tolower))
   pce[bm] <- -19.54 +
     (2.469 * log(age[bm])) + (0.302 * log(tc[bm])) +
-    (-0.307 * log(hdl[bm])) + (1.916 * log(sbp[bm]) * bptx[bm]) +
-    (1.809 * log(sbp[bm]) * as.numeric(bptx[bm]==0)) + (0.549 * smoking[bm]) +
-    (0.645 * dm[bm])
+    (-0.307 * log(hdl[bm])) + (1.916 * log(sbp[bm]) * bprx[bm]) +
+    (1.809 * log(sbp[bm]) * as.numeric(bprx[bm]==0)) + (0.549 * smoke[bm]) +
+    (0.645 * diab[bm])
 
   pce <- exp(pce)
   pce[ww] <- 1 - 0.9665 ^ pce[ww]
@@ -82,4 +103,3 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=0, bl
 
   pce
 }
-
