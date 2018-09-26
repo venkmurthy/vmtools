b60355ffd004fbb03b43a656da153af7a7c876d9
Venkatesh L. Murthy <venkmurthy@gmail.com>
2018-09-26 15:42:02 -0400

    Fix age limitations so that PCE calculator uses 40 for people younger and 79 for people older

--- R/PCEcalc.R
+++ R/PCEcalc.R
@@ -50,6 +50,11 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=c(0,"
 
   # Initialize vectors
   pce <- rep(NA,length(age))
+
+  age2 <- age
+  age2[age2>79] <- 79
+  age2[age2<40] <- 40
+
   bprx <- rep(NA,length(bptx))
   bprx[sapply(bptx,tolower) %in% sapply(bptx.true,tolower)] <- 1
   bprx[sapply(bptx,tolower) %in% sapply(bptx.false,tolower)] <- 0
@@ -63,34 +68,34 @@ calcPCE <- function(age, race, sex, tc, hdl, sbp, bptx, smoking, dm, white=c(0,"
   # White women
   ww <- (sapply(sex,tolower) %in% sapply(female,tolower)) & (sapply(race,tolower) %in% sapply(white,tolower))
   pce[ww] <- 29.18 +
-    (-29.799 * log(age[ww])) + (4.884 * log(age[ww])^2) +
-    (13.540 * log(tc[ww])) + (-3.114 * log(age[ww]) * log(tc[ww])) + (-13.578 * log(hdl[ww])) +
-    (3.149 * log(age[ww]) * log(hdl[ww])) + (2.019 * log(sbp[ww]) * bprx[ww]) +
+    (-29.799 * log(age2[ww])) + (4.884 * log(age2[ww])^2) +
+    (13.540 * log(tc[ww])) + (-3.114 * log(age2[ww]) * log(tc[ww])) + (-13.578 * log(hdl[ww])) +
+    (3.149 * log(age2[ww]) * log(hdl[ww])) + (2.019 * log(sbp[ww]) * bprx[ww]) +
     (1.957 * log(sbp[ww]) * as.numeric(bprx[ww]==0)) + (7.574 * smoke[ww]) +
-    (-1.665 * log(age[ww]) * smoke[ww]) + (0.661 * diab[ww])
+    (-1.665 * log(age2[ww]) * smoke[ww]) + (0.661 * diab[ww])
 
   # Black women
   bw <- (sapply(sex,tolower) %in% sapply(female,tolower)) & (sapply(race,tolower) %in% sapply(black,tolower))
   pce[bw] <- -86.61 +
-    (17.114 * log(age[bw])) + (0.940 * log(tc[bw])) + (-18.920 * log(hdl[bw])) +
-    (4.475 * log(age[bw]) * log(hdl[bw])) + (29.291 * log(sbp[bw]) * bprx[bw]) +
-    (-6.432 * log(age[bw]) * log(sbp[bw]) * bprx[bw]) +
+    (17.114 * log(age2[bw])) + (0.940 * log(tc[bw])) + (-18.920 * log(hdl[bw])) +
+    (4.475 * log(age2[bw]) * log(hdl[bw])) + (29.291 * log(sbp[bw]) * bprx[bw]) +
+    (-6.432 * log(age2[bw]) * log(sbp[bw]) * bprx[bw]) +
     (27.820 * log(sbp[bw]) * as.numeric(bprx[bw]==0)) +
-    (-6.087 * log(age[bw]) * log(sbp[bw]) * as.numeric(bprx[bw]==0)) +
+    (-6.087 * log(age2[bw]) * log(sbp[bw]) * as.numeric(bprx[bw]==0)) +
     (0.691 * smoke[bw]) + (0.874 * diab[bw])
 
   # White men
   wm <- (sapply(sex,tolower) %in% sapply(male,tolower)) & (sapply(race,tolower) %in% sapply(white,tolower))
   pce[wm] <- -61.18 +
-    (12.344 * log(age[wm])) + (11.853 * log(tc[wm])) + (-2.664 * log(age[wm]) * log(tc[wm])) +
-    (-7.990 * log(hdl[wm])) + (1.769 * log(age[wm]) * log(hdl[wm])) + (1.797 * log(sbp[wm]) * bprx[wm]) +
+    (12.344 * log(age2[wm])) + (11.853 * log(tc[wm])) + (-2.664 * log(age2[wm]) * log(tc[wm])) +
+    (-7.990 * log(hdl[wm])) + (1.769 * log(age2[wm]) * log(hdl[wm])) + (1.797 * log(sbp[wm]) * bprx[wm]) +
     (1.764 * log(sbp[wm]) * as.numeric(bprx[wm]==0)) + (7.837 * smoke[wm]) +
-    (-1.795 * log(age[wm]) * smoke[wm]) + (0.658 * diab[wm])
+    (-1.795 * log(age2[wm]) * smoke[wm]) + (0.658 * diab[wm])
 
   # Black men
   bm <- (sapply(sex,tolower) %in% sapply(male,tolower)) & (sapply(race,tolower) %in% sapply(black,tolower))
   pce[bm] <- -19.54 +
-    (2.469 * log(age[bm])) + (0.302 * log(tc[bm])) +
+    (2.469 * log(age2[bm])) + (0.302 * log(tc[bm])) +
     (-0.307 * log(hdl[bm])) + (1.916 * log(sbp[bm]) * bprx[bm]) +
     (1.809 * log(sbp[bm]) * as.numeric(bprx[bm]==0)) + (0.549 * smoke[bm]) +
     (0.645 * diab[bm])
