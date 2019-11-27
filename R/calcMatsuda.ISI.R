bef1468d285d7fb27f51b29b77a13d7a4252bcc3
Venkatesh Murthy <venkmurthy@gmail.com>
2019-11-27 13:33:35 -0500

    rename fasting.insulin to insulin.fasting and likewise for glucose. We need to do this because variable names can't start with a number and we want to be consistent for functions that have glucose.2h for example.

--- R/calcMatsuda.ISI.R
+++ R/calcMatsuda.ISI.R
@@ -3,28 +3,28 @@
 #' This function calculates Matsuda insulin sensitivity index from 2 hour oral glucose tolerance test
 #' Reference: Matsuda M and DeFronzo RA. Diabetes Care 1999; 22:1462-70
 #' DOI: 10.2337/diacare.22.9.1462
-#' @param fasting.insulin in μU/mL
-#' @param fasting.glucose in mass units (mg/dL)
-#' @param mean.insulin in μU/mL
-#' @param mean.glucose in mass units (mg/dL)
+#' @param insulin.fasting in μU/mL
+#' @param glucose.fasting in mass units (mg/dL)
+#' @param insulin.mean in μU/mL
+#' @param glucose.mean in mass units (mg/dL)
 #' @return Matsuda ISI as numeric value
 #' @keywords Matsuda ISI
 #' @examples
 #' calcMatsuda.ISI(15.0,100.0,30,150)
 #'
-#' fasting.glucose.vals <- seq(75,175,by=25)
-#' fasting.insulin.vals <- seq(2,20, by=1)
-#' mean.glucose.vals <- seq(75,175,by=25)
-#' mean.insulin.vals <- seq(2,20, by=1)
-#' test.dat <- expand.grid(fasting.insulin=insulin.vals,fasting.glucose=glucose.vals,
-#'                         mean.insulin=mean.insulin.vals,mean.glucose=mean.glucose.vals)
+#' glucose.fasting.vals <- seq(75,175,by=25)
+#' insulin.fasting.vals <- seq(2,20, by=1)
+#' glucose.mean.vals <- seq(75,175,by=25)
+#' insulin.mean.vals <- seq(2,20, by=1)
+#' test.dat <- expand.grid(insulin.fasting=insulin.vals,glucose.fasting=glucose.vals,
+#'                         insulin.mean=insulin.mean.vals,glucose.mean=glucose.mean.vals)
 #'
-#' test.dat$Matsuda.ISI <- calcMatsuda.ISI(fasting.insulin=test.dat$fasting.insulin,
-#'                                         fasting.glucose=test.dat$fastingglucose,
-#'                                         mean.insulin=test.dat$mean.insulin,
-#'                                         mean.glucose=test.dat$mean.glucose)
+#' test.dat$Matsuda.ISI <- calcMatsuda.ISI(insulin.fasting=test.dat$insulin.fasting,
+#'                                         glucose.fasting=test.dat$fastingglucose,
+#'                                         insulin.mean=test.dat$insulin.mean,
+#'                                         glucose.mean=test.dat$glucose.mean)
 #' @export
 
-calcMatsuda.ISI <- function(fasting.insulin,fasting.glucose,mean.insulin,mean.glucose) {
-  return(10000/sqrt(fasting.insulin*fasting.glucose*mean.insulin*mean.glucose))
+calcMatsuda.ISI <- function(insulin.fasting,glucose.fasting,insulin.mean,glucose.mean) {
+  return(10000/sqrt(insulin.fasting*glucose.fasting*insulin.mean*glucose.mean))
 }
