66b629f9e6bfdde195cabfedf795f702ee2561c9
Venkatesh Murthy <venkmurthy@gmail.com>
2020-08-21 15:08:13 -0400

    deal with special characters in the query string for HMDB searching by metabolite name

--- R/queryHMDB.R
+++ R/queryHMDB.R
@@ -16,6 +16,7 @@ library(dplyr)
 library(rvest)
 library(xml2)
 
+
 HMDB_ID_from_name <- function(met.name) {
   # Convert input met name to lower case
   q <- tolower(met.name)
@@ -30,7 +31,7 @@ HMDB_ID_from_name <- function(met.name) {
 
   # Loop, downloading search results and extracting HMDB IDs
   repeat {
-    p <- read_html(sprintf(search.url,i,gsub(" ","+",q))) %>% html_nodes("div.result-link") %>% html_nodes("a") %>% html_text()
+    p <- read_html(sprintf(search.url,i,URLencode(q,reserved=TRUE))) %>% html_nodes("div.result-link") %>% html_nodes("a") %>% html_text()
 
     hmdb.ids <- c(hmdb.ids, p)
 
